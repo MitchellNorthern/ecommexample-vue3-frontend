c5d8f5ec1fed2eebcd2218a0417b41ec8cd4a7bf
Mitchell Northern <s720414@sbuniv.edu>
2020-01-25 22:02:00 -0600

    Set up example counter(?)

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,19 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //     path: "/about",
-  //     name: "about",
-  //     // route level code-splitting
-  //     // this generates a separate chunk (about.[hash].js) for this route
-  //     // which is lazy-loaded when the route is visited.
-  //     component: () =>
-  //         import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
+    // {
+    //     path: "/about",
+    //     name: "about",
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () =>
+    //         import(/* webpackChunkName: "about" */ "../views/About.vue")
+    // }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
---
+++ src/views/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="incrementCounter">+</button>
+        <div>{{ counter }}</div>
+        <button @click="decrementCounter">-</button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        /* ************* */
+        /* EXAMPLE STATE */
+        /* ************* */
+        const counter = ref(0)
+
+        /* *************** */
+        /* EXAMPLE METHODS */
+        /* *************** */
+        const incrementCounter = () => counter.value++
+        const decrementCounter = () => counter.value--
+    }
+}
+</script>
+
+<style></style>
